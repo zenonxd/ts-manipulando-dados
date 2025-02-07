a14ed27e3c1721b0d8d35f88f3a633e785f2ee65
zenonxd <olavomoreira99@gmail.com>
2025-02-07 16:32:08 -0300

    feat: currency to number

---
+++ src/moedaToNumber.ts
@@ -0,0 +1,10 @@
+
+
+export default function moedaToNumber(moeda: string): number | null {
+
+    const numero = Number(moeda.replace(".", "").replace(",", "."));
+
+    return isNaN(numero) ? null : numero;
+
+
+}
